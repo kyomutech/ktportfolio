5f19b9c0376459c8252d3d07cbdeca5e217db92a
rrojasj <roberto.rojas@kyomutech.com>
2024-10-08 01:32:35 -0600

    Navbar complete.

---
+++ js/general.js
@@ -0,0 +1,21 @@
+'use strict';
+//Menu
+let headerContenedor = document.getElementById('header');
+let botonActivador = document.getElementById('btn-main-menu');
+
+botonActivador.addEventListener('click', () => {
+    headerContenedor.classList.toggle('active');
+});
+
+//Header con clase fixed
+window.onscroll = function() { funcionScroll() };
+let header = document.getElementById('header');
+let headerFixed = header.offsetTop;
+
+const funcionScroll = () => {
+    if (window.pageYOffset > headerFixed) {
+        header.classList.add('fixed');
+    } else {
+        header.classList.remove('fixed');
+    }
+};
