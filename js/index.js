a912eaffac9a7839614c60bf061b840abd4d8b44
rrojasj <roberto.rojas@kyomutech.com>
2024-11-30 04:11:06 -0600

    Index - Fix of main sections - responsiveness

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 // Initialize AutoTyping
 document.addEventListener('DOMContentLoaded', () => {
     autoTypeMainSection();
+    generateStars();
 });
 
 function handleTransition() {
@@ -11,8 +12,7 @@ function handleTransition() {
     // After fade out, set display none and fade in the new section
     setTimeout(function () {
         welcomeSection.style.display = 'none'; // Hide the current section
-        const starsSection = document.querySelector('.stars-section');
-        starsSection.firstElementChild.classList.add('d-flex', 'align-items-center', 'justify-content-center');
+        const starsSection = document.querySelector('.space-section');
         starsSection.classList.remove('d-none'); // Correctly remove the 'd-none' class
         
         // Apply fade-in transition
@@ -59,5 +59,29 @@ function autoTypeMainSection() {
     }, 15000); // This should match the total duration of typing and delay before deleting text
 }
 
+// Function to generate random stars
+function generateStars() {
+    const spaceSection = document.getElementById('space');
+    const starCount = Math.floor(Math.random() * 40) + 30; // Random number of stars (30 to 70)
 
+    for (let i = 0; i < starCount; i++) {
+        const star = document.createElement('div');
+        star.classList.add('star');
 
+        // Random positions
+        const randomTop = Math.random() * 100; // Percentage
+        const randomLeft = Math.random() * 100; // Percentage
+
+        // Apply the random positions
+        star.style.top = randomTop + '%';
+        star.style.left = randomLeft + '%';
+
+        // Randomize star size
+        const randomSize = Math.random() * 3 + 2; // Size between 2px and 5px
+        star.style.width = randomSize + 'px';
+        star.style.height = randomSize + 'px';
+
+        // Append the star to the space section
+        spaceSection.appendChild(star);
+    }
+}
